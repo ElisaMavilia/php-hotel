04b504907a2631e0323705b5385b3e8c2fa59a51
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-02 17:01:10 +0200

    added select

--- views/header.php
+++ views/header.php
@@ -1,5 +1,19 @@
-<header>
-    <h1 class="py-4 ps-5 ">
-                PHP HOTEL
-    </h1>
+<header class="d-flex align-content-center align-items-center">
+    <div id="left-header">
+        <h1 class="py-4 ps-5 text-uppercase">
+            Php Hotel
+        </h1>
+    </div>
+    <div id="rigth-header" class="">
+        <form action="Index.php" method="GET">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </form>
+    </div>
+    
+
 </header>
