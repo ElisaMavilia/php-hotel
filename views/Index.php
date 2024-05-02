f27d61c9abfe0115499bf808dfc8c385ed7fc750
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-02 16:17:28 +0200

    Added table

--- views/Index.php
+++ views/Index.php
@@ -3,11 +3,12 @@
 $template="";
 
 include __DIR__ ."/../models/data.php";
-foreach ($hotels as $hotel){
-        $template .= "<ul> <li>{$hotel['name']}</li> <li>{$hotel['description']}</li> <li>{$hotel['parking']}</li> <li>{$hotel['vote']}</li> <li>{$hotel['distance_to_center']}</li> </ul>";
-    }
-    /* var_dump(($hotels)); */
 
+foreach ($hotels as $hotel){
+        $template .= "<tr><td>{$hotel['name']}</td> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";     
+    }
+
+    /* var_dump($hotels);  */
 ?>
 
 <!DOCTYPE html>
@@ -15,13 +16,34 @@ foreach ($hotels as $hotel){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
     <title>Document</title>
 </head>
 <body>
     <main>
-        <div>
-           <?php echo $template ?>
+        <div class="container mt-4 ">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Hotel</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead> 
+            <tbody>
+                <?php echo $template ?>
+            </tbody>
+        
+          
+        </table>
         </div>
+     
+        
     </main>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
 </body>
 </html>
