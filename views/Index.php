1c9a0d7b7779a153b18db7619e859bdcc9d9db0b
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-02 16:26:10 +0200

    adjustments

--- views/Index.php
+++ views/Index.php
@@ -6,6 +6,10 @@ include __DIR__ ."/../models/data.php";
 
 foreach ($hotels as $hotel){
         $template .= "<tr><td>{$hotel['name']}</td> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";     
+    };
+
+    if ($hotel['parking']=== true){
+       
     }
 
     /* var_dump($hotels);  */
@@ -16,9 +20,9 @@ foreach ($hotels as $hotel){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="view/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-    <title>Document</title>
+    <title>PHP Hotel</title>
 </head>
 <body>
     <main>
