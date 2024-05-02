2099df7c33f71018c02933caf240e9f35b0f2216
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-02 14:42:41 +0200

    init

---
+++ views/Index.php
@@ -0,0 +1,18 @@
+<?php 
+include __DIR__ ."/../models/Data.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        main
+    </main>
+</body>
+</html>
